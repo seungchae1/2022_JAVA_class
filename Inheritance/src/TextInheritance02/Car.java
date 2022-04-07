8860457965eaaa0223d989afad3b94a44e805c86
seungchae <s2125@e-mirim.hs.kr>
2022-04-07 21:53:10 +0900

    Car_truck

--- Inheritance/src/TextInheritance02/Car.java
+++ Inheritance/src/TextInheritance02/Car.java
@@ -1,7 +1,13 @@
 package TextInheritance02;
 
-public class Car {
-	String carname; //차 이름
-	
-
+public class Car{
+	String carname; // 차 이름
+	String color="검정색"; // 색
+	int velocity;
+	void speedUp(){ // 속도 증가하다
+	velocity += 5;
+	}
+	void speedDown(){ // 속도 감소하다
+	velocity -= 5;
+	}
 }
