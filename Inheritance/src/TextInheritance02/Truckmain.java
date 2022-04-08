4fac23d6a8d47d0e5523e68179439c6732080b98
seungchae <s2125@e-mirim.hs.kr>
2022-04-08 09:47:52 +0900

    truck_speed(부모 클래스 메소드 사용)

--- Inheritance/src/TextInheritance02/Truckmain.java
+++ Inheritance/src/TextInheritance02/Truckmain.java
@@ -5,7 +5,13 @@ public class Truckmain {
 		Truck mytruck = new Truck();
 		mytruck.carname = "프론티어";
 		mytruck.ton = 3;
+		mytruck.velocity=100;
 		System.out.println("나의 트럭은 " + mytruck.color + "이다.");
 		System.out.println(mytruck.carname+"는 "+mytruck.ton +"톤을 실을 수 있다");
+		System.out.println("트럭의 속도는 "+mytruck.velocity + "이다");
+		mytruck.speedUp();
+		System.out.println("트럭의 속도는 "+mytruck.velocity + "로 5증가했다");
+		mytruck.speedDown();
+		System.out.println("트럭의 속도는 "+mytruck.velocity + "로 5감소했다");
 	}
 }
