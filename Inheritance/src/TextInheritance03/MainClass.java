b18631aceb7434f309a59c385c5bc6c66a061b75
seungchae <s2125@e-mirim.hs.kr>
2022-04-08 10:07:38 +0900

    leader를 false로 바꿔보기

--- Inheritance/src/TextInheritance03/MainClass.java
+++ Inheritance/src/TextInheritance03/MainClass.java
@@ -3,7 +3,8 @@ package TextInheritance03;
 public class MainClass {
 
 	public static void main(String[] args) {
-		Leader hong=new Leader("ȫ�浿", "30130", true);
+		//Leader hong=new Leader("ȫ�浿", "30130", true);
+		Leader hong = new Leader("ȫ�浿", "30130", false);
 		hong.isLeader();
 	}
 
