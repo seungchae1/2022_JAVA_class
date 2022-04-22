b06ce8d706c3adbc5f35031e4198824bb131f8c2
seungchae <s2125@e-mirim.hs.kr>
2022-04-22 11:31:25 +0900

    Wrapper클래스_Integer_equals()

--- PakageProject/src/package03/IntegerEqualExam.java
+++ PakageProject/src/package03/IntegerEqualExam.java
@@ -17,5 +17,9 @@ public class IntegerEqualExam {
 		in2=300;
 		System.out.println("in1.equals(in2) ==> "+in1.equals(in2));
 	}
+	//Wrapper클래스를 사용하는 이유
+	//1. java.util패키지의 클래스들은 객체 데이터만 처리
+	//2. ArrayList, Collection 프레임 워크의 구조는 객체 타입만 저장(자료구조)
+	//3. 멀티스레드에서 동기화를 지원하려면 
 
 }
