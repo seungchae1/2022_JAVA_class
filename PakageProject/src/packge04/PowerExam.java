de564153fef8208d74a44eb42172c808279fd490
seungchae <s2125@e-mirim.hs.kr>
2022-04-26 11:38:28 +0900

    Math.pow(a,b)/a의 b제곱(실수)

---
+++ PakageProject/src/packge04/PowerExam.java
@@ -0,0 +1,12 @@
+package packge04;
+
+public class PowerExam {
+
+	public static void main(String[] args) {
+		System.out.println(Math.pow(3, 2));
+		System.out.println(Math.pow(3.0f, 2.0f));
+		System.out.println(Math.pow(5.0, 2));
+		System.out.println(Math.pow(-5.0, 3.0));
+	}
+
+}
