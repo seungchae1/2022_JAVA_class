92d0c0b3ca937844a5446f30436542ea25e66020
seungchae <s2125@e-mirim.hs.kr>
2022-04-26 11:41:40 +0900

    Math.sqrt(루트값)

---
+++ PakageProject/src/packge04/SquareExam.java
@@ -0,0 +1,12 @@
+package packge04;
+
+public class SquareExam {
+
+	public static void main(String[] args) {
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.sqrt(9.0f));
+		System.out.println(Math.sqrt(9.0));
+		System.out.println(Math.sqrt(-9.0));	//°á°ú´Â NaN(Not a Number)
+	}
+
+}
