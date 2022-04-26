fe8c8c8237fdb048bb067f137b1e212dffbcd8f1
seungchae <s2125@e-mirim.hs.kr>
2022-04-26 11:34:40 +0900

    Math.abs(절대값)

---
+++ PakageProject/src/packge04/AbsExam.java
@@ -0,0 +1,13 @@
+package packge04;
+
+public class AbsExam {
+
+	public static void main(String[] args) {
+		System.out.println(Math.abs(25));	// abs는 absolute의 약어로, 절대값을 나타낸다.
+		System.out.println(Math.abs(-25));
+		System.out.println(Math.abs(25.5));
+		System.out.println(Math.abs(-25.5));
+
+	}
+
+}
